236c4f73dc8bf0efe7d327312dd1b4552398e512
09ania09 <asadovska@st.swps.edu.pl>
2025-01-10 13:25:21 +0100

    lab8

--- myproject/myapp/serializers.py
+++ myproject/myapp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Person, Team, MONTHS, SHIRT_SIZES
+from .models import Person, Team, Osoba, MONTHS, SHIRT_SIZES
 
 
 class PersonSerializer(serializers.Serializer):
@@ -36,3 +36,11 @@ class PersonSerializer(serializers.Serializer):
         instance.team = validated_data.get('team', instance.team)
         instance.save()
         return instance
+    fields = ['id', 'firstname', 'lastname', 'monyh_added', 'shirt_size', 'team']
+    read_only_fields = ['id']
+    
+class OsobaModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Osoba
+        fields = '__all__'
+        read_only_fields = ['id']
